7c401bef91750fe87dafb43b9c51521922a991e6
pduchesne <pduchesne@gmail.com>
2024-06-09 21:25:22 +0200

    fix solid auth asynchronicity

--- src/solid/auth.tsx
+++ src/solid/auth.tsx
@@ -1,11 +1,19 @@
 import {useSession} from "./SessionProvider";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {PromiseContainer} from "@hilats/react-utils";
+import {Navigate} from "react-router-dom";
 
 export function SolidAuth(props: {}) {
     const session = useSession();
-    session.handleIncomingRedirect({restorePreviousSession: true});
 
-    return <div>
-Loading
-    </div>
+    const [authResponse, setAuthResponse] = useState<Promise<any>>();
+
+    useEffect(() => {
+        setAuthResponse(session.handleIncomingRedirect({restorePreviousSession: true}));
+    }, [session]);
+
+
+    return authResponse ? <PromiseContainer promise={authResponse}>
+        {() => <Navigate to={'/personal/dashboard'} />}
+    </PromiseContainer> : null
 }
